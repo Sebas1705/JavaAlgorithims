0373be213298f3e604ac39a765b85dd78c2fa508
Sebas1705 <sebssgarcia502580@gmail.com>
2022-10-05 19:46:01 +0200

    Array_List

--- algorithims-java/src/main/java/com/sebss/App.java
+++ algorithims-java/src/main/java/com/sebss/App.java
@@ -1,6 +1,8 @@
 package com.sebss;
 
-import com.sebss.DataStructures.Lists.Linked_List.*;
+import java.util.ArrayList;
+
+import com.sebss.DataStructures.Lists.Array_List.*;
 
 /**
  * Hello world!
@@ -10,26 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        DoubleCircularLinked_List<Integer> list = new DoubleCircularLinked_List<Integer>();
-        for(int i=0;i<20;i++)list.add(i);
-        list.add(0,100);
-        list.add(4,1000);
-        list.add(100);
-        list.set(12,0);
+        Array_List<Integer> list = new Array_List<Integer>(1,1,2,4,45,2,2,2,2,1);
         list.printAsArray();
-
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-
-        DoubleCircularLinked_List<Integer> list2 = new DoubleCircularLinked_List<Integer>();
-        list2.add(1001);
-        list2.add(1010);
-        list2.addAll(2,list);
-        list2.remove(1001);
-        list2.removeAt(list2.size()-1);
-        list2.printAsArray();
-        list2.getLast().print();
-
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-
+        System.out.println(list.lastIndexOf(12));
+        list.printAsArray();
     }
 }
