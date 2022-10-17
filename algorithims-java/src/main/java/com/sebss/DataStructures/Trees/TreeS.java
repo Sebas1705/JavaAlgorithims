36c118e7ba366f157039aa8fa6961bf75c4e09ee
Sebas1705 <sebssgarcia502580@gmail.com>
2022-10-17 20:18:14 +0200

    Trees 2

--- algorithims-java/src/main/java/com/sebss/DataStructures/Trees/TreeS.java
+++ algorithims-java/src/main/java/com/sebss/DataStructures/Trees/TreeS.java
@@ -4,18 +4,16 @@ import com.sebss.DataStructures.Interfaces.CollectionS;
 import com.sebss.DataStructures.Lists.ListS;
 
 public interface TreeS<T> {
+    public int getHight();
+    public int getSize();
+    public int getOrder();
+    public int getGrade();
+    public int isPerfect();
+    public TreeS<T> getSubTree(T e);
+    public TreeS<T> getSubTree(int hight, int grade);
     public boolean add(T e);
-    public boolean addAll(CollectionS<T> c);
-    public boolean remove(T e);
-    public boolean removeAll(CollectionS<T> c);
-    public boolean retainAll(CollectionS<T> c);
-    public void add(int i, T e);
-    public boolean addAll(int i, CollectionS<T> c);
-    public T get(int i);
-    public int indexOf(T e);
-    public int lastIndexOf(T e);
-    public T removeAt(int i);
-    public T set(int i, T e);
-    public ListS<T> subList(int fi, int ti);
-    public void printAsArray();
+    public boolean add(T e, T inE);
+    public int[] search(T e);
+    public boolean removeSubArbol(T e);
+    public boolean removeSubArbol(int hight, int grade);
 }
